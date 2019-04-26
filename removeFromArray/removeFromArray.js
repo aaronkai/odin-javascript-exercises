16425cf93fe4424708826a7454e3c62375698414
aaron.kai.hubbard <aaron.kai.hubbard@gmail.com>
2019-04-26 10:42:20 -0400

    start of 4/26

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,18 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(providedArray, valueToRemove) {
+    if (arguments.length == 2) {
+      let index = providedArray.indexOf(valueToRemove)
+      return newProvidedArray.splice(index, 1)
+    }
+    else {
+      let newProvidedArray = arguments.shift();
+      for (let i = 0; i < arguments.length; i++) {}
+        index = providedArray.indexOf(valueToRemove);
+        newProvidedArray.splice(index, 1)
+    }
+        
+      
+    }
 }
 
+console.log(removeFromArray([4,3,2,1], 3))
 module.exports = removeFromArray
