65caea36b395927d2fa325ab3c433355f408939f
aaron.kai.hubbard <aaron.kai.hubbard@gmail.com>
2019-04-29 14:35:29 -0400

    finished exercises

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(temperature) {
+  let conversion = ((temperature - 32) * (5/9));
+  return parseFloat(conversion.toFixed(1));
 }
 
-const ctof = function() {
-
+const ctof = function(temperature) {
+  let conversion = ((temperature * (9/5)) + 32);
+  return parseFloat(conversion.toFixed(1));
 }
 
 module.exports = {
